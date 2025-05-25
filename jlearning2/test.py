7063138e8c3c2d10b8028db79fe79413982710d1
Saw Min <minthetkhie195@gmai.com>
2025-05-25 20:31:52 +0900

    Test in test file

--- jlearning2/test.py
+++ jlearning2/test.py
@@ -1,10 +1,17 @@
 import re
 
-result = re.search(r"\d+", "There are 243 apples")
-#\d means "any digit" (0-9).
-# + means "one or more times".
-# So, \d+ matches one or more digits in a row (e.g., "24").
-if result:
-    print(f"Match found: {result.group()}") #
-else:
-    print("No match found.")
+# result = re.search(r"\d+", "There are 243 apples")
+# #\d means "any digit" (0-9).
+# # + means "one or more times".
+# # So, \d+ matches one or more digits in a row (e.g., "24").
+# if result:
+#     print(f"Match found: {result.group()}") #
+# else:
+#     print("No match found.")
+
+import re
+
+text = "My phone number is 123-456-7890."
+match = re.search(r"\d{3}-\d{3}-\d{4}", text)
+if match:
+    print("Found phone number:", match.group())
