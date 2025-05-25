05fbfeb36c840935222992e28ac7ba3af21e965f
Saw Min <minthetkhie195@gmai.com>
2025-05-25 20:25:17 +0900

    adding comments

--- jlearning2/test.py
+++ jlearning2/test.py
@@ -1,7 +1,10 @@
 import re
 
 result = re.search(r"\d+", "There are 243 apples")
+#\d means "any digit" (0-9).
+# + means "one or more times".
+# So, \d+ matches one or more digits in a row (e.g., "24").
 if result:
-    print(f"Match found: {result.group()}")
+    print(f"Match found: {result.group()}") #
 else:
     print("No match found.")
